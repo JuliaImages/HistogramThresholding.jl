96ec671aaf3119ec03a11873630f48ae3f4166b9
Wesley <wesleytws@gmail.com>
2018-12-22 00:13:23 +1030

    Add minimum error method

--- src/HistogramThresholding.jl
+++ src/HistogramThresholding.jl
@@ -11,16 +11,17 @@ struct Bimodal <: HistogramDistribution end
 
 abstract type ThresholdAlgorithm end
 struct Otsu <: ThresholdAlgorithm end
+struct Minerror <: ThresholdAlgorithm end
 
 include("otsu.jl")
-
-
+include("minerror.jl")
 
 
 export
 	# main functions
     find_threshold,
-	Otsu
+	Otsu,
+	Minerror
 # =======
 # >>>>>>> Stashed changes
 end # module
