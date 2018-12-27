a1742c0fdc83a9800d1387109e2c8ea5b222c17b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-27 03:21:59 +0000

    build based on 382adfb

---
+++ dev/search_index.js
@@ -0,0 +1,27 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "HistogramThresholding.jl Documentation",
+    "title": "HistogramThresholding.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#HistogramThresholding.find_threshold-Tuple{Otsu,AbstractArray,AbstractRange}",
+    "page": "HistogramThresholding.jl Documentation",
+    "title": "HistogramThresholding.find_threshold",
+    "category": "method",
+    "text": "t = find_threshold(Otsu(),  histogram, edges)\n\nUnder the assumption that the histogram is bimodal the threshold is set so that the resultant inter-class variance is maximal.\n\nArguments\n\nThe function arguments are described in more detail below.\n\nhistogram\n\nAn AbstractArray storing the frequency distribution.\n\nedges\n\nAn AbstractRange specifying how the intervals for the frequency distribution are divided.\n\nReference\n\nNobuyuki Otsu (1979). \"A threshold selection method from gray-level histograms\". IEEE Trans. Sys., Man., Cyber. 9 (1): 62–66. doi:10.1109/TSMC.1979.4310076\n\n\n\n\n\n"
+},
+
+{
+    "location": "#HistogramThresholding.jl-Documentation-1",
+    "page": "HistogramThresholding.jl Documentation",
+    "title": "HistogramThresholding.jl Documentation",
+    "category": "section",
+    "text": "find_threshold(::Otsu,  ::AbstractArray, ::AbstractRange)"
+},
+
+]}
