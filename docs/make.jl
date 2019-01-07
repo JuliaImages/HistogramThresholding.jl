1739b06a9d3138259ee5f1fff8b10a93f09f1982
Tristan <betttris13@yahoo.com.au>
2019-01-07 13:27:15 +1030

    fixed test case

--- docs/make.jl
+++ docs/make.jl
@@ -2,4 +2,4 @@ push!(LOAD_PATH,"../src/")
 using Documenter, HistogramThresholding
 makedocs(sitename="Documentation",
             Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"))
-deploydocs(repo = "github.com/betttris13/HistogramThresholding.jl.git")
+deploydocs(repo = "github.com/zygmuntszpak/HistogramThresholding.jl.git")
