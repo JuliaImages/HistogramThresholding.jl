8e20b600f0e9a567cd1fbb5fb8c12c5ca8ee6a8f
Dr. Zygmunt L. Szpak <zygmunt.szpak@gmail.com>
2019-05-29 22:53:32 +0930

    Removes REQUIRE file and updates Project.toml
    Removes upper bound on Images package and removes Julia 0.7 tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
-using HistogramThresholding, Images, TestImages
-using Test
+using HistogramThresholding, TestImages, Test, ImageCore, ColorTypes, ImageContrastAdjustment
+
 
 function gaussian_mixture(z;P₁,μ₁,σ₁,P₂,μ₂,σ₂)
     (P₁ / (σ₁*√(2*π))) * exp( -(z-μ₁)^2 / (2*σ₁^2) )  +  (P₂ / (σ₂*√(2*π))) * exp( -(z-μ₂)^2 / (2*σ₂^2) )
